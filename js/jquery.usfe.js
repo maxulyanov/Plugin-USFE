a71b69ba44eb27df967479a45e0675f9d422a4b7
M-Ulyanov <web.ulyanov@gmail.com>
2014-05-27 02:28:07 +0400

    Version 1.1.3
    Вызывать плагин стало еще удобней!
    Добавлены 3 пользовательских параметра
    Мелкие изменения

--- js/jquery.usfe.js
+++ js/jquery.usfe.js
@@ -11,6 +11,12 @@
 			f_editButtonName : 'Изменить',
 			f_charDelete : '&#215;',
 			f_bgDelete: '#f48475',
+
+			//settings type = number
+			n_iconPlus: '+',
+			n_iconMinus: '-',
+			n_editBg: true,
+
 		};
 
 		options = $.extend(defaults, options);
@@ -137,8 +143,8 @@
 
 				// (3.1.1)
 				var numWrap = $('<div class="number-wrap">'),
-					numPlus = $('<button type="button" value="-" class="number-plus">+</button>'),
-					numMinus = $('<button type="button" value="-" class="number-minus">-</button>');
+					numPlus = $('<button type="button" value="-" class="number-plus">'+ defaults.n_iconPlus +'</button>'),
+					numMinus = $('<button type="button" value="-" class="number-minus">'+ defaults.n_iconMinus +'</button>');
 
 				$(elem).wrap(numWrap);
 				$(elem).before(numMinus).after(numPlus);
@@ -150,33 +156,43 @@
 					var thisVal = $(thisInput).val();
 					thisVal++;
 
-					$(thisInput).stop(true, true).animate({
-						backgroundColor: '#9bcb1e',
-					},400).stop(true,true).animate({
-						backgroundColor: '#FFF',
-					},400);
+					// (3.1.3)
+					if(defaults.n_editBg){
+						$(thisInput).stop(true, true).animate({
+							backgroundColor: '#9bcb1e',
+						},400).stop(true,true).animate({
+							backgroundColor: '#FFF',
+						},400);
+					}
 
 					computation(this, thisVal);
 				});
 
-				// (3.1.3)
+				// (3.1.4)
 				$('.number-minus').on('click', function(){
+
 					var thisInput = $(this).parent().find('input[data-element="number"]');
 					var thisVal = $(thisInput).val();
 					thisVal--; 
 
+					// (3.1.5)
 					if(!thisVal) thisVal = 1;
-					$(thisInput).stop(true, true).animate({
-						backgroundColor: '#ea6856',
-					},400).stop(true,true).animate({
-						backgroundColor: '#FFF',
-					},400);
+
+					// (3.1.6)
+					if(defaults.n_editBg){
+						$(thisInput).stop(true, true).animate({
+							backgroundColor: '#ea6856',
+						},400).stop(true,true).animate({
+							backgroundColor: '#FFF',
+						},400);
+					}
 
 					computation(this, thisVal);
 				});
 
 				// (3.1.4)
 				$('input[data-element="number"]').on('keypress input change', function(event){
+
 					if(event.charCode < 48 || event.charCode > 57) return false;
 					var thisVal = $(this).parent().find('input[data-element="number"]').val(); 
 
@@ -185,9 +201,11 @@
 			 
 				// (3.1.5)
 				function computation(elem, val){
+
 					if(isNaN(val)){
 						val = 1;
 					}
+
 					$(elem).parent().find('input[data-element="number"]').val(val);
 					$(elem).parent().find('input[data-element="number"]').attr('value', val);
 				}
@@ -204,14 +222,26 @@
 
 		return this.each(function(){
 
-			var type = $(this).attr('type');
-			var dataEl = $(this).attr('data-element');
-
-			if(type == 'file'){
-				methods.typeFile(this);
+			if(this.nodeName.toLowerCase() == 'form'){
+				$(this).children().each(function(){
+					definition(this);
+				});
 			}
-			else if(type == 'text'){
-				(dataEl == 'number') ? methods.typeNumber(this) : methods.typeText(this);
+			else{
+				definition(this);
+			}
+
+			function definition(el){
+				
+				var type = $(el).attr('type');
+				var dataEl = $(el).attr('data-element');
+
+				if(type == 'file'){
+					methods.typeFile(el);
+				}
+				else if(type == 'text'){
+					(dataEl == 'number') ? methods.typeNumber(el) : methods.typeText(el);
+				}
 			}
 
 		});
